916efaa81ea97cfaffb2a6401ce5c4bb5db16f93
degortes <danilodegortes@gmal.com>
2020-12-22 17:02:09 +0100

    section prodotti ok

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -3,6 +3,61 @@
 
 @section('content')
 
-<h1>Le Lunghe</h1>
+<section id="products">
+    <div class="container">
+        <h2>Le Lunghe</h2>
+        <div class="card-container">
+            @foreach ($pasta as $type)
+                @if ($type['tipo']== 'lunga')
+
+                <div class="card">
+                    <img src="{{$type['src'] }}" alt="">
+                    <div class="hide-card">
+                        <a href="#">{{$type['titolo'] }}</a>
+                    </div>
+                </div>
+                @endif
+
+            @endforeach
+
+        </div>
+        <h2>Le corte</h2>
+        <div class="card-container">
+            @foreach ($pasta as $type)
+                @if ($type['tipo']== 'corta')
+
+                <div class="card">
+                    <img src="{{$type['src'] }}" alt="">
+                    <div class="hide-card">
+                        <a href="#">{{$type['titolo'] }}</a>
+                    </div>
+                </div>
+                @endif
+
+            @endforeach
+
+        </div>
+        <h2>Le cortissime</h2>
+        <div class="card-container">
+            @foreach ($pasta as $type)
+                @if ($type['tipo'] == 'cortissima')
+
+                <div class="card">
+                    <img src="{{$type['src'] }}" alt="">
+                    <div class="hide-card">
+                        <a href="#">{{$type['titolo'] }} ciao</a>
+                    </div>
+                </div>
+                @endif
+
+            @endforeach
+
+        </div>
+
+    </div>
+
+</section>
+
+
 
 @endsection
