02759e5b41934cd56cc4b2b4cecb2dc7d321cee1
degortes <danilodegortes@gmal.com>
2020-12-22 17:28:32 +0100

    macro footer ok

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,88 @@
 <footer>
-    <h1>footer</h1>
+    <div class="container">
+        <nav>
+            <div class="footer-logo">
+                <div class="logo">
+                    <img src="{{asset('images/logo-la-molisana.png')}}" alt="">
+                </div>
+                <p>Ragione sociale <br> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco<br> laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate<br> velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat <br>non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+            </div>
+            <div class="foot-nav">
+                <h3>Pastificio</h3>
+                <ul>
+                    <li>
+                        <a href="#">Il Pastificio</a>
+                    </li>
+                    <li>
+                        <a href="#">Il Pastificio</a>
+                    </li>
+                    <li>
+                        <a href="#">Il Pastificio</a>
+                    </li>
+                    <li>
+                        <a href="#">Il Pastificio</a>
+                    </li>
+                    <li>
+                        <a href="#">Il Pastificio</a>
+                    </li>
+                    <li>
+                        <a href="#">Il Pastificio</a>
+                    </li>
+                </ul>
+
+            </div>
+            <div class="foot-nav">
+                <h3>Pastificio</h3>
+                <ul>
+                    <li>
+                        <a href="#">Il Pastificio</a>
+                    </li>
+                    <li>
+                        <a href="#">Il Pastificio</a>
+                    </li>
+                    <li>
+                        <a href="#">Il Pastificio</a>
+                    </li>
+                    <li>
+                        <a href="#">Il Pastificio</a>
+                    </li>
+                    <li>
+                        <a href="#">Il Pastificio</a>
+                    </li>
+                    <li>
+                        <a href="#">Il Pastificio</a>
+                    </li>
+                </ul>
+
+            </div>
+            <div class="foot-nav">
+                <h3>Pastificio</h3>
+                <ul>
+                    <li>
+                        <a href="#">Il Pastificio</a>
+                    </li>
+                    <li>
+                        <a href="#">Il Pastificio</a>
+                    </li>
+                    <li>
+                        <a href="#">Il Pastificio</a>
+                    </li>
+                    <li>
+                        <a href="#">Il Pastificio</a>
+                    </li>
+                    <li>
+                        <a href="#">Il Pastificio</a>
+                    </li>
+                    <li>
+                        <a href="#">Il Pastificio</a>
+                    </li>
+                </ul>
+
+            </div>
+
+        </nav>
+
+    </div>
+
 </footer>
